1c7ab6ec3cfb5fbd8b8d24136aefe118d20ca553
enu-44 <enuarmunozcastillo@gmail.com>
2024-02-29 22:22:03 -0500

    se optimiza la creacion de monedas

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -4,10 +4,14 @@ import { Scene } from "phaser";
 export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
-    platform: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+
+    platforms: Phaser.Types.Physics.Arcade.ImageWithDynamicBody[] = [];
+
     player: Phaser.Physics.Arcade.Sprite; // Cambio a Sprite para manejar animaciones
     cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined; // Cambio aquí
+
     scoreText: Phaser.GameObjects.Text; // Nuevo atributo para el texto del puntaje
+    score: number = 0;
 
     playerSpeed = 500;
     jumpVelocity = -500; // Velocidad vertical inicial del salto
@@ -16,10 +20,8 @@ export class Game extends Scene {
     backgroundWidth = 6048; // Ancho del fondo
     backgroundHeight = 768; // Altura del fondo
     backgroundScale = 1.5; // Escala del fondo para que cubra toda la pantalla
-    
 
     numberOfPlatforms = 5; // Número de plataformas que deseas agregar
-    score: number = 0;
 
     constructor() {
         super("Game");
@@ -27,49 +29,43 @@ export class Game extends Scene {
     create() {
         this.camera = this.cameras.main;
         this.physics.world.setBoundsCollision(true, false, true, true);
-        this.createAnimations()
-        this.createBackground()
-        this.createPlatformBush()
-        this.createPlayer()
-        this.createScore()
-        this.createPlatforms()
-        this.createCamera()
+        this.createAnimations();
+        this.createBackground();
+        this.createPlatformBush();
+        this.createPlayer();
+        this.createScore();
+        this.createPlatforms();
+        this.createCoins();
+        this.createCamera();
         this.cursors = this.input.keyboard?.createCursorKeys();
         EventBus.emit("current-scene-ready", this);
     }
 
-
     createScore() {
-        this.scoreText = this.add.text(10, 10, `Score: ${this.score}`, {
-            fontFamily: 'Arial Black',
-            fontSize: 38,
-            color: '#ffffff',
-            stroke: '#000000',
-            strokeThickness: 8,
-            align: 'left'
-        }).setDepth(100);
-    }
-
-    // Método para actualizar el texto del puntaje
-    updateScoreText() {
-        if (this.scoreText) {
-            this.scoreText.setText(`Score: ${this.score}`);
-        }
+        this.scoreText = this.add
+            .text(10, 10, `Score: ${this.score}`, {
+                fontFamily: "Arial Black",
+                fontSize: 38,
+                color: "#ffffff",
+                stroke: "#000000",
+                strokeThickness: 8,
+                align: "left",
+            })
+            .setDepth(100);
     }
 
     incrementScore() {
         // Incrementa la puntuación del jugador
         this.score += 10; // Incrementa la puntuación en 10 puntos por cada moneda
-        this.updateScoreText();
+        this.scoreText.setText(`Score: ${this.score}`);
     }
 
     createPlatformBush() {
         const platformY = 420; // Posición Y común para todas las plataformas
-        const bush =  this.add.image(455, platformY, "bush");
+        const bush = this.add.image(455, platformY, "bush");
         bush.setScale(1.5); // Escala el arbusto al 50%
     }
 
-   
     createPlatforms() {
         const platformY = 720; // Posición Y común para todas las plataformas
         const platformX = 455; // Posición X inicial
@@ -80,23 +76,28 @@ export class Game extends Scene {
             platformWidth = platform.width;
             platform.setImmovable();
             platform.setCollideWorldBounds(true);
-
-            //Agregar Moneda
-            const coin = this.createCoin(x)
-            this.physics.add.collider(this.player,  platform);
-            this.physics.add.collider(coin, platform);
+            this.platforms.push(platform);
         }
+        this.physics.add.collider(this.player, this.platforms)
     }
 
-    createCoin(x: number): Phaser.Types.Physics.Arcade.ImageWithDynamicBody {
-        //Monedas
-        const coin = this.physics.add.image(x, this.player.y - 50, "coin"); // Usar la posición Y del jugador
-        coin.setScale(0.05);
-        coin.setData("isCoin", true);
-        coin.setCollideWorldBounds(true);
-        coin.setGravityY(0);
-        coin.setBounce(0.3)
-        return coin;
+    createCoins() {
+        const stars = this.physics.add.group({
+            key: "coin",
+            repeat: 5,
+            setXY: { x: 250, y: 0, stepX: 800 },
+        });
+        stars.children.iterate(function (child) {
+            const value = child as Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+            value.setData("isCoin", true);
+            value.setScale(0.05);
+            value.setCollideWorldBounds(true);
+            value.setBounceY(
+                Phaser.Math.FloatBetween(0.1, 0.4)
+            ); // Usa una aserción de tipo para decirle a TypeScript que child es un ImageWithDynamicBody
+            return true;
+        });
+        this.physics.add.collider(stars, this.platforms);
     }
     createCamera() {
         // Mover el fondo a una velocidad diferente para el efecto parallax
@@ -116,7 +117,7 @@ export class Game extends Scene {
         this.player = this.physics.add.sprite(50, 0, "dude");
         this.player.setCollideWorldBounds(true);
         this.player.setBounce(0.2);
-        this.player.setScale(2)
+        this.player.setScale(2);
     }
 
     createBackground() {
@@ -174,6 +175,7 @@ export class Game extends Scene {
             this.player.setVelocityY(this.jumpVelocity);
         }
 
+        //Aplicar movimeinto al score
         if (this.scoreText) {
             const x = this.player.x - this.cameras.main.width / 2 + 10;
             this.scoreText.setPosition(x, 10);
@@ -182,8 +184,11 @@ export class Game extends Scene {
         // Detecta la superposición con las monedas
         this.children.each((coin: any) => {
             // Verifica si el objeto es una moneda
-            if (coin.getData("isCoin") && this.physics.overlap(this.player, coin)) {
-                alert("Aqui pregunta ?")
+            if (
+                coin.getData("isCoin") &&
+                this.physics.overlap(this.player, coin)
+            ) {
+                // alert("Aqui pregunta ?");
                 // Cuando el jugador recoge una moneda
                 coin.destroy(); // Elimina la moneda del juego
                 // Incrementa la puntuación del jugador
