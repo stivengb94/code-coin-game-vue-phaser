2fb67d6fbd2fe71860bf9d6329d54ebb626b9ec5
Jose Luis Perdomo Andrade <jose.perdomo@interedes.com.co>
2024-03-06 22:13:25 -0500

    implementar vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import App from './App.vue';
 import { createApp } from 'vue';
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-createApp(App).mount('#app');
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app');
