ab4a15166320c551581c8356f2e1e3ba5450360c
Jose Luis Perdomo Andrade <jose.perdomo@interedes.com.co>
2024-03-10 14:36:01 -0500

    merge formularios in main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import App from './App.vue';
 import { createApp } from 'vue';
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import router from './routes/routes';
 
-createApp(App).mount('#app');
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(router).use(vuetify).mount('#app');
---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+      <router-link to="/">Inicio</router-link>
+      <router-link to="/about">Acerca de</router-link>
+    </div>
+</template>
+  
